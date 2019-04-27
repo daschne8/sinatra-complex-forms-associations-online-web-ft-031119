f1b19b288eed8fa214a290a59129160a3e4db459
Daniel Schneider <daschne8@gmail.com>
2019-04-27 17:39:20 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,7 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
+    @pet.owner = Owner.find(params[:owner])
     redirect to "pets/#{@pet.id}"
   end
 
